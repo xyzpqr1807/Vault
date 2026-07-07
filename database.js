cb6c8f54a9453d9aee013fe45d0251c251bcd7de
xyzpqr1807 <xyzpqr1807@gmail.com>
2026-07-07 23:50:45 +0530

    Add files via upload

---
+++ database.js
@@ -0,0 +1,12 @@
+// Add your exact photo filenames here (must be inside the 'images' folder)
+const ourPhotos = [
+    "photo1.jpg",
+    "photo2.jpg",
+    "photo3.png" // Add a comma and new line for each new photo
+];
+
+// Add your exact video filenames here (must be inside the 'videos' folder)
+const ourVideos = [
+    "video1.mp4",
+    "video2.mp4"
+];
