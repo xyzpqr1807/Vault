9baf78b671a42614f1fb9760f24c580a9447edaf
xyzpqr1807 <xyzpqr1807@gmail.com>
2026-07-08 01:25:23 +0530

    Add files via upload

---
+++ database.js
@@ -0,0 +1,18 @@
+// Add your exact photo filenames here (must be inside the 'images' folder)
+const ourPhotos = [
+    "photo1.jpg",
+    "photo2.jpg",
+    "photo3.png" // Add a comma and new line for each new photo
+];
+
+// Add your exact video filenames here (must be inside the 'videos' folder)
+const ourVideos = [
+    "video1.mp4",
+    "video2.mp4"
+];
+
+// Add your YouTube Video IDs here
+const ourMixtape = [
+    "rNXmANj72ps", // Tere Pass Main
+    "vuVn1r0DpeE"  // Your Lata Mangeshkar song!
+];
